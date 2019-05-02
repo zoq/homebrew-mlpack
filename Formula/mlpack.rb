f39213251e9a6b5941bc6e5d8803a853f50bb944
Marcus Edel <marcus.edel@fu-berlin.de>
2019-05-02 19:29:16 +0200

    Add initial mlpack formula.

---
+++ Formula/mlpack.rb
@@ -0,0 +1,82 @@
+class Mlpack < Formula
+  desc "Scalable C++ machine learning library"
+  homepage "http://www.mlpack.org"
+  # doi "arXiv:1210.6293"
+  url "https://kurg.org/data/mlpack-0.0.tar.gz"
+  sha256 "04456163653a53e4104c1acb6758694948e41833c865a8757313ab95c0c28172"
+
+  bottle do
+    cellar :any
+    #sha256 "e9d8e97e3df78d48ccfff553fa46cfc6d61204133ce8bf86ead2022d43716ac4" => :high_sierra
+    #sha256 "dee66abdc8257caba52d37b69bd2e5514d9a697841182553cc309424913e7739" => :sierra
+    #sha256 "0bba3b4d17b38c2ca882b7f09fa694095674ef27853ffb140fc514cd143512e9" => :el_capitan
+    #sha256 "8a435c0e52162b282cb23bfb0b5f62fecbba5bc0bc2b87f1c5b89a11617af8be" => :x86_64_linux
+  end
+
+  option "with-debug", "Compile with debug options"
+  option "with-profile", "Compile with profile options"
+  option "with-arma-extra-debug", "Compile with extra Armadillo debugging symbols"
+  option "with-test-verbose", "Run test cases with verbose output"
+  option "with-build-tests", "Build tests"
+  option "with-build-cli-executables", "Build command-line executables"
+  option "with-build-python-bindings", "Build Python bindings"
+
+  # Build dependencies.
+  depends_on "cmake" => :build
+  depends_on "pkg-config"
+  depends_on "armadillo"
+  depends_on "boost"
+
+  # Documentation dependencies.
+  depends_on "graphviz"
+  depends_on "doxygen"
+
+  def install
+    dylib = OS.mac? ? "dylib" : "so"
+    cmake_args = std_cmake_args
+    cmake_args << "-DDEBUG=" + (build.with?("debug") ? "ON" : "OFF")
+    cmake_args << "-DPROFILE=" + (build.with?("profile") ? "ON" : "OFF")
+    cmake_args << "-DBOOST_ROOT=#{Formula["boost"].opt_prefix}"
+    cmake_args << "-DARMADILLO_INCLUDE_DIR=#{Formula["armadillo"].opt_include}"
+    cmake_args << "-DARMADILLO_LIBRARY=#{Formula["armadillo"].opt_lib}/libarmadillo.#{dylib}"
+    cmake_args << "-DCMAKE_CXX_FLAGS=-fext-numeric-literals" unless ENV.compiler == :clang
+
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make", "install"
+    end
+
+    doc.install Dir["doc/*"]
+    pkgshare.install "src/mlpack/tests" # Includes test data.
+  end
+
+  test do
+    cd testpath do
+      system "#{bin}/mlpack_knn",
+        "-r", "#{pkgshare}/tests/data/GroupLensSmall.csv",
+        "-n", "neighbors.csv",
+        "-d", "distances.csv",
+        "-k", "5", "-v"
+    end
+
+    (testpath / "test.cpp").write <<-EOS
+      #include <mlpack/core.hpp>
+
+      using namespace mlpack;
+
+      int main(int argc, char** argv) {
+        Log::Debug << "Compiled with debugging symbols." << std::endl;
+        Log::Info << "Some test informational output." << std::endl;
+        Log::Warn << "A false alarm!" << std::endl;
+      }
+      EOS
+    cxx_with_flags = ENV.cxx.split + ["test.cpp",
+                                      "-std=c++11",
+                                      "-I#{include}",
+                                      "-I#{Formula["armadillo"].opt_lib}/libarmadillo",
+                                      "-L#{lib}", "-lmlpack",
+                                      "-o", "test"]
+    system *cxx_with_flags
+    system "./test", "--verbose"
+  end
+end
